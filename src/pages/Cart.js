9a8c9f61ad490e4c414bd1763c9a57b9f0200efb
Gonzalo Ventura <gonzalogventura@gmail.com>
2022-08-05 18:50:07 -0300

    Actualizacion de carrito de compras

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,10 +1,28 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container'
+import React, { useContext } from 'react'
+import {Container, Row, Col, Card} from 'react-bootstrap'
+import { CartContext } from '../context/CartContext';
+import CartProduct from '../components/CartProduct/CartProduct'
+import DiscountCode from '../components/DiscountCode/DiscountCode'
 
 const Cart = () => {
+  const { cartProducts } = useContext(CartContext)
+  console.log("cart", cartProducts)
   return (
     <Container className='app_container'>
         <h1>Carrito</h1>
+        <Row>
+          <Col xl={8}><CartProduct data={cartProducts} /></Col>
+          <Col xl={4}>
+            <Card className='cart__card__right'>
+              <div className='card__discountcode'>
+                <h6>Ingresar cupón de descuento</h6>
+                <DiscountCode />
+              </div>
+              
+            </Card>
+          </Col>
+        </Row>
+        
     </Container>
   )
 }
