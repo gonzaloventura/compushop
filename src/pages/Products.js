6641343fa2620cd1a9d1d2a7a14e11897fb3182c
Gonzalo Ventura <gonzalogventura@gmail.com>
2022-08-04 23:01:21 -0300

    Pagina de productos

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -6,7 +6,7 @@ import ItemListContainer from '../components/ItemListContainer/ItemListContainer
 function Contact() {
   return (
     <Container className='app_container'>
-        <ItemListContainer section='Productos destacados'/>
+        <ItemListContainer section='Productos'/>
     </Container>
   )
 }
