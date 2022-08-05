25f8a4d970348d5bbb1c4220cd208bab19fc905f
Gonzalo Ventura <gonzalogventura@gmail.com>
2022-08-04 23:01:07 -0300

    Creación de página de checkout

---
+++ src/pages/Checkout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const Checkout = () => {
+  return (
+    <Container className='app_container'>
+        <h1>Checkout</h1>
+    </Container>
+  )
+}
+
+export default Checkout
